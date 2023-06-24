ed5351886c6853f83894a7d5977f0ed4717a0244
Rohan Vidhate <49265832+roneya@users.noreply.github.com>
2023-06-24 12:33:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,70 +1,66 @@
-const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
-const quoteDisplayElement = document.getElementById('quoteDisplay')
-const quoteInputElement = document.getElementById('quoteInput')
-const timerElement = document.getElementById('timer')
+//your JS code here. If required.
+// Fetching a random quote from the API
+async function getRandomQuote() {
+  const response = await fetch("http://api.quotable.io/random");
+  const data = await response.json();
+  return data.content;
+}
 
-const area={}
-quoteInputElement.addEventListener('input', () => {
-  const arrayQuote = quoteDisplayElement.querySelectorAll('span')
-  const arrayValue = quoteInputElement.value.split('')
+const quoteDisplayElement = document.getElementById("quote");
+const quoteInputElement = document.getElementById("quoteInput");
+const timerElement = document.querySelector(".timer");
+let startTime, timerInterval;
 
-  let correct = true
-  arrayQuote.forEach((characterSpan, index) => {
-    const character = arrayValue[index]
-    if (character == null) {
-      characterSpan.classList.remove('correct')
-      characterSpan.classList.remove('incorrect')
-      correct = false
-    } else if (character === characterSpan.innerText) {
-      characterSpan.classList.add('correct')
-      characterSpan.classList.remove('incorrect')
-    } else {
-      characterSpan.classList.remove('correct')
-      characterSpan.classList.add('incorrect')
-      correct = false
-    }
-  })
-
-  // if (correct) renderNewQuote()
-  console.log(arrayQuote.length,arrayValue.length)
-  if(arrayQuote.length<=arrayValue.length){
-    console.log(arrayQuote.length,arrayValue.length)
-    clearInterval(area.timer)
-
-    setTimeout(renderNewQuote,3000)
-  }
-})
-
-function getRandomQuote() {
-  return fetch(RANDOM_QUOTE_API_URL)
-    .then(response => response.json())
-    .then(data => data.content)
+function startTimer() {
+  timerInterval = setInterval(() => {
+    const elapsedTime = new Date(Date.now() - startTime);
+    const minutes = elapsedTime.getMinutes().toString().padStart(2, "0");
+    const seconds = elapsedTime.getSeconds().toString().padStart(2, "0");
+    const milliseconds = elapsedTime.getMilliseconds().toString().padStart(3, "0");
+    timerElement.textContent = `${minutes}:${seconds}:${milliseconds}`;
+  }, 10);
 }
 
 async function renderNewQuote() {
-  const quote = await getRandomQuote()
-  quoteDisplayElement.innerHTML = ''
-  // quoteDisplayElement.innerText = quote
-  quote.split('').forEach(character => {
-    const characterSpan = document.createElement('span')
-    characterSpan.innerText = character
-    quoteDisplayElement.appendChild(characterSpan)
-  })
-  quoteInputElement.value = null
-  startTimer()
+  const quote = await getRandomQuote();
+  quoteDisplayElement.innerHTML = "";
+  quote.split("").forEach(character => {
+    const characterSpan = document.createElement("span");
+    characterSpan.innerText = character;
+    quoteDisplayElement.appendChild(characterSpan);
+  });
+  quoteInputElement.value = null;
+  startTimer();
 }
 
-let startTime
-function startTimer() {
-  timerElement.innerText = 0
-  startTime = new Date()
-  area.timer = setInterval(() => {
-    timer.innerText = getTimerTime()
-  }, 1000)
+function stopTimer() {
+  clearInterval(timerInterval);
+  timerElement.textContent = "00:00:00";
 }
 
-function getTimerTime() {
-  return Math.floor((new Date() - startTime) / 1000)
-}
+renderNewQuote();
 
-renderNewQuote()
+quoteInputElement.addEventListener("input", () => {
+  const quoteArray = quoteDisplayElement.querySelectorAll("span");
+  const inputArray = quoteInputElement.value.split("");
+  let correct = true;
+  quoteArray.forEach((characterSpan, index) => {
+    const character = inputArray[index];
+    if (character == null) {
+      characterSpan.classList.remove("correct");
+      characterSpan.classList.remove("incorrect");
+      correct = false;
+    } else if (character === characterSpan.innerText) {
+      characterSpan.classList.add("correct");
+      characterSpan.classList.remove("incorrect");
+    } else {
+      characterSpan.classList.remove("correct");
+      characterSpan.classList.add("incorrect");
+      correct = false;
+    }
+  });
+  if (correct) {
+    stopTimer();
+    setTimeout(renderNewQuote, 3000);
+  }
+});
